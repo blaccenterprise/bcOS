8ebab7fa665c4ebb74598b08d3b764b218804f4b
Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>
2026-08-15 22:16:29 -0700

    Add files via upload

---
+++ ASE/Message/Sonney_Message.js
@@ -0,0 +1,165 @@
+
+                    function myFunction() {
+                                              //Variables {
+                        var text;
+                        var consciousness = document.getElementById("output").value;
+                        var ASE = new Artyom();
+                        var emergency = 1;
+                        var relax = 2;
+                        //}
+
+                        switch (consciousness) {
+                            
+//Digital Modules  {
+
+   
+
+    //Knowledge Module {
+    
+        //Message {
+            //Stevie Carpenter {
+                            //A.S.E. Spoken Language  {
+                            case "Message Arthenia":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1mBNJj1LpGlHjEDByUX1133V63yW7vHRx_w3lZsFgKoM/edit?usp=sharing", "https://docs.google.com/document/d/1mBNJj1LpGlHjEDByUX1133V63yW7vHRx_w3lZsFgKoM/edit?usp=sharing", "width=400,height=400");
+                                break;
+                            //}
+                            //Shorthand - Written Language  {
+                            case "message arthenia":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1mBNJj1LpGlHjEDByUX1133V63yW7vHRx_w3lZsFgKoM/edit?usp=sharing", "https://docs.google.com/document/d/1mBNJj1LpGlHjEDByUX1133V63yW7vHRx_w3lZsFgKoM/edit?usp=sharing", "width=400,height=400");
+                                break;
+                            //}
+                            //Default - Written Language  {
+                            case "Message Arthenia":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1mBNJj1LpGlHjEDByUX1133V63yW7vHRx_w3lZsFgKoM/edit?usp=sharing", "https://docs.google.com/document/d/1mBNJj1LpGlHjEDByUX1133V63yW7vHRx_w3lZsFgKoM/edit?usp=sharing", "width=400,height=400");
+                                break;
+                            //}
+                            //} /* Channge To Brycee */
+            //Larry Caldwell  {
+                            //A.S.E. Spoken Language  {
+                            case "Message Lotti":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1MlIxGI55NUNhmeuo-Iw9UjzIrkEPOvMJwq8WVRikiPg/edit?usp=sharing", "https://docs.google.com/document/d/1MlIxGI55NUNhmeuo-Iw9UjzIrkEPOvMJwq8WVRikiPg/edit?usp=sharing", "width=400,height=400");
+                                break;
+                            //}
+                            //Shorthand - Written Language  {
+                            case "message lotti":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1MlIxGI55NUNhmeuo-Iw9UjzIrkEPOvMJwq8WVRikiPg/edit?usp=sharing", "https://docs.google.com/document/d/1MlIxGI55NUNhmeuo-Iw9UjzIrkEPOvMJwq8WVRikiPg/edit?usp=sharing", "width=400,height=400");
+                                break;
+                            //}
+                            //Default - Written Language  {
+                            case "Message Lotti":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1MlIxGI55NUNhmeuo-Iw9UjzIrkEPOvMJwq8WVRikiPg/edit?usp=sharing", "https://docs.google.com/document/d/1MlIxGI55NUNhmeuo-Iw9UjzIrkEPOvMJwq8WVRikiPg/edit?usp=sharing", "width=400,height=400");
+                                break;
+                            //}
+                            //}
+            //Daymeion Sheild  {
+                            //A.S.E. Spoken Language  {
+                            case "Message chubby Rock":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1ylIeFIqmA3tYhmBGFIuP7vd7snPyXxPsZKCv-F465tk/edit?usp=sharing", "https://docs.google.com/document/d/1ylIeFIqmA3tYhmBGFIuP7vd7snPyXxPsZKCv-F465tk/edit?usp=sharing", "width=400,height=400");
+                                break;
+                            //}
+                            //Shorthand - Written Language  {
+                            case "message chubby Rock":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1ylIeFIqmA3tYhmBGFIuP7vd7snPyXxPsZKCv-F465tk/edit?usp=sharing", "https://docs.google.com/document/d/1ylIeFIqmA3tYhmBGFIuP7vd7snPyXxPsZKCv-F465tk/edit?usp=sharing", "width=400,height=400");
+                                break;
+                            //}
+                            //Default - Written Language  {
+                            case "Message Chubby Rock":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1ylIeFIqmA3tYhmBGFIuP7vd7snPyXxPsZKCv-F465tk/edit?usp=sharing", "https://docs.google.com/document/d/1ylIeFIqmA3tYhmBGFIuP7vd7snPyXxPsZKCv-F465tk/edit?usp=sharing", "width=400,height=400");
+                                break;
+                            //}
+                            //}
+            //David Bush  {
+                            //A.S.E. Spoken Langugae  {
+
+                            case "Message Sunny":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1odudNCupUO0iLYjsqgocMlAYz1L4tzJjtyVOW5gTJos/edit?usp=sharing", "https://docs.google.com/document/d/1odudNCupUO0iLYjsqgocMlAYz1L4tzJjtyVOW5gTJos/edit?usp=sharing", "width=400,height=400");
+                                break;
+
+                            case "Message Sonney":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1odudNCupUO0iLYjsqgocMlAYz1L4tzJjtyVOW5gTJos/edit?usp=sharing", "https://docs.google.com/document/d/1odudNCupUO0iLYjsqgocMlAYz1L4tzJjtyVOW5gTJos/edit?usp=sharing", "width=400,height=400");
+                                break;
+                            //}
+                            //Shorthand - Written Langugae  {
+
+                            case "message sunny":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1odudNCupUO0iLYjsqgocMlAYz1L4tzJjtyVOW5gTJos/edit?usp=sharing", "https://docs.google.com/document/d/1odudNCupUO0iLYjsqgocMlAYz1L4tzJjtyVOW5gTJos/edit?usp=sharing", "width=400,height=400");
+                                break;
+
+                            case "message sonney":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1odudNCupUO0iLYjsqgocMlAYz1L4tzJjtyVOW5gTJos/edit?usp=sharing", "https://docs.google.com/document/d/1odudNCupUO0iLYjsqgocMlAYz1L4tzJjtyVOW5gTJos/edit?usp=sharing", "width=400,height=400");
+                                break;
+                            //}
+                            //Default - Written Langugae  {
+
+                            case "Message Sunny":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1odudNCupUO0iLYjsqgocMlAYz1L4tzJjtyVOW5gTJos/edit?usp=sharing", "https://docs.google.com/document/d/1odudNCupUO0iLYjsqgocMlAYz1L4tzJjtyVOW5gTJos/edit?usp=sharing", "width=400,height=400");
+                                break;
+
+                            case "Message Sonney":
+                                text = "Welcome";
+                                ASE.say("Welcome");
+                                window.open("https://docs.google.com/document/d/1odudNCupUO0iLYjsqgocMlAYz1L4tzJjtyVOW5gTJos/edit?usp=sharing", "https://docs.google.com/document/d/1odudNCupUO0iLYjsqgocMlAYz1L4tzJjtyVOW5gTJos/edit?usp=sharing", "width=400,height=400");
+                                break;
+                            //}
+                            //}
+                            //}
+                            
+
+//}
+
+                            //}
+
+                            //Conditional  {
+                            case "bring up Mass alert":
+                                text = "bring up Mass alert";
+                                ASE.say("Okay. Stand by.");
+                                if (emergency < relax) {
+                                    alert("Emergency");
+                                    window.open("https://blaccenterprise.github.io/blacc_profile/Contact_Card/Business_Mass_Alert.html", "https://blaccenterprise.github.io/blacc_profile/Contact_Card/Business_Mass_Alert.html", "width=400,height=400");
+                                } else {
+                                    alert("Relax");
+                                    window.open("https://blaccenterprise.github.io/blacc_profile/Contact_Card/David_Bush.html", "https://blaccenterprise.github.io/blacc_profile/Contact_Card/David_Bush.html", "width=400,height=400");
+
+                                };
+                                break;
+
+                            //}
+                            //reponse to not being able to hear me
+
+                            default:
+                                text = "My apologies.";
+                                ASE.say("I could not hear you clearly King Carpenter can you please repeat that");
+                                break;
+                        }
+
+                        document.getElementById("demo").innerHTML = text;
+                    }
+
