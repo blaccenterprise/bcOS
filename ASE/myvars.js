d436fb869da05af949a09aaf613c49118f31e86f
Stevie W. Carpenter <181901167+blaccenterprise@users.noreply.github.com>
2026-08-15 20:55:15 -0700

    Add files via upload

---
+++ ASE/myvars.js
@@ -0,0 +1,106 @@
+
+                                                        
+    case "where is Baldwin Park":
+    text = "where is Baldwin Park.";
+    Arthenya.say("Baldwin Park is in Los Angeles California of District 1 next to El Monte, Irwindale, West Covina and La Puente.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is Covina":
+    text = "where is Covina.";
+    Arthenya.say("Covina is in Los Angeles California of District 1 next to Azusa and West Covina.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is West Covina":
+    text = "where is West Covina.";
+    Arthenya.say("West Covina is in Los Angeles California of District 1 next to Covina, Baldwin Park, La Puente, Industry and Walnut.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is Walnut":
+    text = "where is Walnut.";
+    Arthenya.say("Walnut is in Los Angeles California of District 1 next to West Covina, Pomona, Diamond Bar and Industry.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is South El Monte":
+    text = "where is South El Monte.";
+    Arthenya.say("South El Monte is in Los Angeles California of District 1 next to El Monte, Industry and Rosemead.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is Industry":
+    text = "where is Industry.";
+    Arthenya.say("Industry is in Los Angeles California of District 1 next to Diamond Bar, Pomona, Walnut, West Covina, La Puente,Baldwin Park, El Monte and South El Monte.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is Pomona":
+    text = "where is Pomona.";
+    Arthenya.say("Pomona is in Los Angeles California of District 1 next to Walnut, Industry and Diamond Bar.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is Rosemead":
+    text = "where is Rosemead.";
+    Arthenya.say("Rosemead is in Los Angeles California of District 1 next to Monterey Park, Alhambra, San Gabriel, El Monte and South El Monte.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is La Puente":
+    text = "where is La Puente.";
+    Arthenya.say("La Puente is in Los Angeles California of District 1 next to Industry and West Covina.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is Montobello":
+    text = "where is Montobello.";
+    Arthenya.say("Montobello is in Los Angeles California of District 1 next to Monterey Park.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is San Gabriel":
+    text = "where is San Gabriel.";
+    Arthenya.say("San Gabriel is in Los Angeles California of District 1 next to Alhambra, Monterey Park and Rosemead.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is Diamond Bar":
+    text = "where is Diamond Bar.";
+    Arthenya.say("Diamond Bar is in Los Angeles California of District 1 next to Industry and Pomona.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is El Monte":
+    text = "where is El Monte.";
+    Arthenya.say("El Monte is in Los Angeles California of District 1 next to Rosemead, South El Monte, Industry, Baldwin Park and Irwindale.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is Irwindale":
+    text = "where is Irwindale.";
+    Arthenya.say("Irwindale is in Los Angeles California of District 1 next to El Monte, Baldwin Park and Azusa.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
+                                                        
+    case "where is Monterey Park":
+    text = "where is Monterey Park.";
+    Arthenya.say("Monterey Park is in Los Angeles California of District 1 next to Montobello, Alhambra, San Gabriel and Rosemead.");
+    window.open("https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "https://blacccconnect-1.w3spaces.com/Level_1_Access/California/Los_Angeles_County/District_1_Level_1_Access.html", "width=400,height=400");
+    break;
+    
